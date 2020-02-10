a67670bfbbee040547d45de08b1badbfa573c7e2
sreenandinimallela <mallelasreenandini2@gmail.com>
2020-02-10 06:41:30 +0000

    Added empty README.md and logback logging

--- src/main/java/com/test/bpdts/heruokuapp/controllers/LocationController.java
+++ src/main/java/com/test/bpdts/heruokuapp/controllers/LocationController.java
@@ -2,7 +2,10 @@ package com.test.bpdts.heruokuapp.controllers;
 
 import com.test.bpdts.heruokuapp.components.DistanceCalculator;
 import com.test.bpdts.heruokuapp.models.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +16,7 @@ import java.util.stream.Collectors;
 
 @RestController
 public class LocationController {
+    private static Logger logger = LoggerFactory.getLogger(LocationController.class);
 
     @Autowired
     RestTemplate restTemplate;
@@ -20,12 +24,15 @@ public class LocationController {
     @Autowired
     DistanceCalculator distanceCalculator;
 
-    @RequestMapping(value = "/")
-    public List<User> getUsers() {
-        List<User> londonUsers = Arrays.asList(restTemplate.getForEntity("https://bpdts-test-app.herokuapp.com/city/London/users", User[].class).getBody());
+    @RequestMapping(value = "/city/{name}")
+    public List<User> getUsers(@PathVariable("name") String name) {
+        List<User> cityUsers = Arrays.asList(restTemplate.getForEntity("https://bpdts-test-app.herokuapp.com/city/" + name + "/users", User[].class).getBody());
         List<User> allUsers = Arrays.asList(restTemplate.getForEntity("https://bpdts-test-app.herokuapp.com/users", User[].class).getBody());
-        List<User> doubleList = londonUsers.stream().flatMap(it -> allUsers.stream().filter(ti -> distanceCalculator.distance(it.getLatitude(), it.getLongitude(), ti.getLatitude(), ti.getLongitude()) <= 50)).collect(Collectors.toList());
-
+        long start = System.currentTimeMillis();
+        List<User> doubleList = cityUsers.parallelStream().flatMap(it -> allUsers.stream().filter(ti -> distanceCalculator.distance(it.getLatitude(), it.getLongitude(), ti.getLatitude(), ti.getLongitude()) <= 50)).collect(Collectors.toList());
+        long end = System.currentTimeMillis();
+        long timeTaken = end - start;
+        logger.info("Processed in ms: {}",timeTaken);
         return doubleList;
 
     }

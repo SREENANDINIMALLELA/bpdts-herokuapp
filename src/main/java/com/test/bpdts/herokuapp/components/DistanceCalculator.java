96bca7291c34da2169178beb0231e92cf363cb43
sreenandinimallela <mallelasreenandini2@gmail.com>
2020-02-10 07:06:16 +0000

    Added tests

--- src/main/java/com/test/bpdts/herokuapp/components/DistanceCalculator.java
+++ src/main/java/com/test/bpdts/herokuapp/components/DistanceCalculator.java
@@ -3,8 +3,7 @@ package com.test.bpdts.herokuapp.components;
 import org.springframework.stereotype.Component;
 
 @Component
-public
-class DistanceCalculator {
+public class DistanceCalculator {
     public Double distance(Double lat1, Double lon1, Double lat2, Double lon2) {
         if ((lat1 == lat2) && (lon1 == lon2)) {
             return 0.0;
@@ -12,7 +11,9 @@ class DistanceCalculator {
             double theta = lon1 - lon2;
             double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
             dist = Math.acos(dist);
-            return (dist * 3958.8);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            return (dist);
         }
     }
 }
